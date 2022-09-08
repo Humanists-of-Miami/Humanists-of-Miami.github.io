16db2c972f657c16b68ac080c288b218d982b51f
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2022-09-08 16:14:14 -0400

    Add CMS configuration

--- postbuild.sh
+++ postbuild.sh
@@ -2,3 +2,4 @@ sudo npm install -g sass
 /bin/cp -ar ./mokk/assets ./gh-pages
 /bin/cp -ar ./mokk/robots.txt ./gh-pages
 sass ./mokk/assets/css/main.scss ./gh-pages/assets/css/main.css
+/bin/cp -ar ./mokk/admin/config.yml ./gh-pages/admin/config.yml
