a4437ab77db689b32b4c8f65704daa5aa230072b
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2023-04-01 19:13:14 -0400

    Add comments section

--- postbuild.sh
+++ postbuild.sh
@@ -2,4 +2,5 @@ sudo npm install -g sass
 /bin/cp -ar ./mokk/assets ./gh-pages
 /bin/cp -ar ./mokk/robots.txt ./gh-pages
 sass ./mokk/assets/css/main.scss ./gh-pages/assets/css/main.css
+sass ./mokk/assets/css/bf.scss ./gh-pages/assets/css/bf.css
 /bin/cp -ar ./mokk/admin/config.yml ./gh-pages/admin/config.yml
